f72645b83ae0fb6cbef6e7d7985466a23d498564
rmsa42 <rui.soares.almeida@outlook.pt>
2025-05-21 10:53:16 +0100

    Disabled Lazy update notifications. Ensure typescript lsp is installed in mason

--- nvim/lua/dotfiles/plugins/treesitter.lua
+++ nvim/lua/dotfiles/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },  
