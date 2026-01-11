a1571195d6e258e934cab7db47dcb765c835aecc
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-11 20:54:55 +0000

    Deleting .configs from the tree-sitter

--- nvim/lua/dotfiles/plugins/treesitter.lua
+++ nvim/lua/dotfiles/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		build = ":TSUpdate",
 		
 		config = function () 
-			local configs = require("nvim-treesitter.configs")
+			local configs = require("nvim-treesitter")
 
 			configs.setup({
 				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "bash" },
