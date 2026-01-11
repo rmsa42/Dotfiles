dfae00b32a97b46b9d287a1a68dee75e8ff45f59
rmsa42 <rumachad@NiceGood.lan>
2026-01-11 19:35:23 +0000

    Adding a space in treesitter

--- nvim/lua/dotfiles/plugins/treesitter.lua
+++ nvim/lua/dotfiles/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 			local configs = require("nvim-treesitter.configs")
 
 			configs.setup({
-				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "bash"},
+				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "bash" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },  
