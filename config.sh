869ab7190a774e931e0697cf766e9812e607517b
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-11 23:16:45 +0000

    Fixed config script

--- config.sh
+++ config.sh
@@ -1,26 +1,33 @@
-apt-get install -y curl zsh npm clangd
+sudo apt-get install -y curl zsh npm clangd
+mkdir -p bin
+mkdir -p Programs
 
 # Set up global .gitconfig file
-echo -n "Github email:"
+GITCONFIG_FILE="$HOME/.gitconfig"
+echo "Setup gitconfig"
+echo -n "Github email: "
 read email
-cp ./gitconfig.txt ~/.gitconfig
+cp ./gitconfig.txt $GITCONFIG_FILE
 git config --global user.email $email
 
 cd $HOME
 
-# Install oh-my-zsh via curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir -p bin
-mkdir -p Programs
-
 # Set up nvim config
+BIN="$HOME/bin"
 NVIM_BIN="nvim0.11.0"
-wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage -O $(NVIM_BIN)
-chmod +x $(NVIM_BIN)
-mv bin/ $(NVIM_BIN)
-if [ -z $XDG_CONFIG_HOME ]; then
-	cp -r ./nvim ~/.config/.
-else
-	cp -r ./nvim $XDG_CONFIG_HOME/.
+if [[ ! -a $BIN ]]; then
+	echo "Setup nvim"
+	wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage -O $(NVIM_BIN)
+	chmod +x $(NVIM_BIN)
+	mv $BIN $(NVIM_BIN)
+	if [ -z $XDG_CONFIG_HOME ]; then
+		cp -r ./nvim ~/.config/.
+	else
+		cp -r ./nvim $XDG_CONFIG_HOME/.
+	fi
 fi
 npm install -g tree-sitter-cli
+
+# Install oh-my-zsh via curl
+echo "Setup oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
