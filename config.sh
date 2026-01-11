fdf125e3603316035f7e946d018d1b9b2def10a0
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-11 19:18:08 +0000

    Updated config.sh script
    Now installs zsh and configures the .zshrc

--- config.sh
+++ config.sh
@@ -12,3 +12,11 @@ if [ -z $XDG_CONFIG_HOME ]; then
 else
 	ln -sf $PWD/nvim $XDG_CONFIG_HOME/.
 fi
+
+cd $HOME
+apt-get install -y curl zsh
+
+# Install oh-my-zsh via curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir -p bin
+echo 'export PATH="$PATH:$HOME/bin"' >> .zshrc
