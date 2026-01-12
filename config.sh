44958fa87a80bb52f5dc6372a2ba4444ab355870
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-12 00:01:23 +0000

    Fixing config script

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+DOTFILES=$(pwd)
 BIN="$HOME/bin"
 
 sudo apt-get install -y curl zsh npm clangd
@@ -12,21 +13,21 @@ read email
 cp ./gitconfig.txt $GITCONFIG_FILE
 git config --global user.email $email
 
-cd $HOME
-
 # Set up nvim config
-NVIM_BIN="$BIN/nvim0.11.0"
+NVIM_BIN="$BIN/nvim"
 if [[ ! -a $NVIM_BIN ]]; then
 	echo "Setup nvim"
 	wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.appimage -O $NVIM_BIN
 	chmod +x $NVIM_BIN
 	if [ -z $XDG_CONFIG_HOME ]; then
-		cp -r ./nvim ~/.config/.
+		cp -r $DOTFILES/nvim ~/.config/.
 	else
-		cp -r ./nvim $XDG_CONFIG_HOME/.
+		cp -r $DOTFILES/nvim $XDG_CONFIG_HOME/.
 	fi
 fi
 sudo npm install -g tree-sitter-cli
+
+cd $HOME
 
 # Install oh-my-zsh via curl
 echo "Setup oh-my-zsh"
