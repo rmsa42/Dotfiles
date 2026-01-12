71491dd389af751d2ddb353d67b88f3a15582206
rmsa42 <rui.soares.almeida@outlook.pt>
2026-01-12 01:32:12 +0000

    Changed treesitter to the master branch

--- nvim/lua/dotfiles/plugins/treesitter.lua
+++ nvim/lua/dotfiles/plugins/treesitter.lua
@@ -1,17 +1,16 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		
-		config = function () 
-			local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
 
-			configs.setup({
-				ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "bash" },
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },  
-			})
-		end
-	}
+	config = function () 
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { "c", "cpp", "asm", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "bash" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },  
+		})
+	end
 }
